dee99f64b2abc7ee45bf200bce5ff645537a867d
irshad321123321 <alidanish5138@gmail.com>
2026-05-07 10:04:23 +0530

    backend folder ready

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import auth from "./module/auth/auth.router";
+import auth from "./modules/auth/auth.router";
 const app = express();
 
 app.use(cors());
--- server/src/module/core-service/reasoning/reasoning.service.ts
+++
@@ -1,9 +0,0 @@
-// export const generateTasks = async (query: string) => {
-//   const response = await geminiClient();
-//   return response;
-// };
-
-// export const generateSubTasks = async (tasks: string[]) => {
-//   const response = await geminiClient();
-//   return response;
-// };
